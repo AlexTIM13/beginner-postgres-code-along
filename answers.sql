57361869819bc19ad8be36261f682187d089ecf7
AlexTIM13 <alejandro.partid@gmail.com>
2021-08-09 20:14:02 -0500

    3carga

--- answers.sql
+++ answers.sql
@@ -6,3 +6,14 @@
 --SELECT count (*)
 --FROM track
 --WHERE milliseconds > 299000;
+
+--SELECT AVG(milliseconds) FROM track;
+
+--SELECT count(*) FROM invoice
+--WHERE billing_country = 'USA';
+
+--SELECT * FROM artist
+--WHERE name LIKE '%A%t%';
+
+--SELECT * FROM artist
+--WHERE name LIKE 'A%t';
